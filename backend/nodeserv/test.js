51286f87bef46d500988e238de0a6aac5398b2bf
willtree8 <willtree8@gmail.com>
2021-12-07 15:28:34 +0000

    fixed sumthin

--- backend/nodeserv/test.js
+++ backend/nodeserv/test.js
@@ -1,9 +1,43 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://willtree8:I5AMZgyX8SWUtbQu@cluster0.xqec2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+require('dotenv').config({ path: './config.env' });
+const uri = process.env.URI
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object   
-  console.log("connected")
-  client.close();
-});
+
+// The database to use
+const dbName = "timetables";
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        
+        // Use the collection "people"
+        const col = db.collection("days");
+        
+        // Construct a document                                                                                                                                                              
+        let personDocument = {
+            "name": { "first": "Alan", "last": "Turing" },
+            "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+            "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+            "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+            "views": 1250000
+        }
+        
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(personDocument);
+        // Find one document
+        const myDoc = await col.findOne();
+        // Print to the console
+        console.log(myDoc);
+        
+    } catch (err) {
+        console.log(err.stack);
+    }
+    
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
